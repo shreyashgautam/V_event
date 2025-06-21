d23721c36a9ee685705bcb4961f9d6feeb7a205a
Shreyash Gautam <shreyashgautam2007@gmail.com>
2025-06-22 03:51:57 +0530

    changing due to cors

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://v-event-phi.vercel.app/", // your frontend
+    origin: "https://v-event-phi.vercel.app", // your frontend
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
